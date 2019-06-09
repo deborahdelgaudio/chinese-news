4caa5bda029744faa911b753df1632c55a2f2526
DeborahDelGaudio <delgaudio.d@gmail.com>
2019-06-09 18:56:50 +0200

    Changed cursor for prepared statement query

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector.cursor import MySQLCursorPrepared
 from mysql.connector import Error
 
 
@@ -28,13 +29,14 @@ class DatabaseManager(object):
                 database=self.config['DATABASE'],
                 port=self.config['PORT'],
                 user=self.config['USER'],
-                password=self.config['PASSWORD']
+                password=self.config['PASSWORD'],
+                use_pure=True
             )
         except Error as err:
             print(str(err))
             raise err
         else:
-            self.cursor = self.connection.cursor(dictionary=True, buffered=True)
+            self.cursor = self.connection.cursor(cursor_class=MySQLCursorPrepared)
             return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -48,14 +50,23 @@ class DatabaseManager(object):
         print("\nClosing connection to {} Database".format(self.config['DATABASE']))
         self.connection.close()
 
-    def execute_query(self, query):
+    def execute_query(self, query, input=None):
         """
         :param self:
         :param query: string -> query to execute
         :return: records
         """
-        print("\nFetching records with the following query: \n{}".format(query))
-        self.cursor.execute(query)
+        print("\nFetching records with the following query: \n{query} \n{input}".format(query=query, input=input))
+
+        if input == None:
+            self.cursor.execute(query)
+        else:
+            self.cursor.execute(query, input)
+
         records = self.cursor.fetchall()
 
         return records
+
+    def update(self):
+        #self.connection.commit()
+        pass
